b79ad19fdd0c49bcb3a3a84e21f063ae8d67b937
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-08-26 01:10:16 -0700

    Time: 22 ms (98.55%), Space: 46.6 MB (91.12%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        LinkedList<List<Integer>> res = new LinkedList<List<Integer>>();
+        
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int target = 0 - nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
+                
+                while(left < right) {
+                    if(nums[left] + nums[right] == target) {
+                        ArrayList<Integer> solution = new ArrayList<>();
+                        solution.add(nums[left]);
+                        solution.add(nums[right]);
+                        solution.add(nums[i]);
+                        res.add(solution);
+                        while(left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if(nums[left] + nums[right] > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
