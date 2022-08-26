09c7db485ba4046e48e65d17b0216b93b9c29e43
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-08-26 01:05:01 -0700

    Time: 45 ms (61.51%), Space: 118.9 MB (14.33%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,11 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        // Initializes the min Heap
         PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> 
             Integer.compare((a[0]*a[0]+a[1]*a[1]), (b[0]*b[0]+b[1]*b[1])));
+        
+        // Adds the points from input matrix to heap
         for(int[] p : points) {
             queue.add(p);
         }
         
+        // Initializes Matrix set to have k rows of 2 columns (x,y) coordinates
+        // Will the k points closest to the origin (0,0)
         int[][] ans = new int[k][2];
         for(int i = 0; i < k; i++) {
             int[] curr = queue.poll();
