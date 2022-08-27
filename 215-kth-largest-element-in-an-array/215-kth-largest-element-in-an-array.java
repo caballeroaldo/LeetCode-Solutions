c66ce7216ec382c446919da71bc88a7fe7125f75
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-08-27 14:06:57 -0700

    Time: 10 ms (72.79%), Space: 73.7 MB (30.43%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickselect(nums, 0, nums.length - 1, nums.length - k);
+    }
+    
+    private int quickselect(int[] arr, int left, int right, int k) {
+        if(left == right) {
+            return arr[left];
+        }
+        
+        int pivot = new Random().nextInt(right - left + 1)  + left;
+        pivot = partition(arr, left, right, pivot);
+        
+        if(pivot == k) {
+            return arr[k];
+        } else if(pivot < k) {
+            return quickselect(arr, pivot + 1, right, k);
+        }
+        return quickselect(arr, left, pivot - 1, k);
+        
+    }
+    
+    private int partition(int[] arr, int left, int right, int pivot) {
+        int pIndex = arr[pivot];
+        swap(arr, pivot, right);
+        pIndex = left;
+        
+        for(int i = left; i <= right; i++ ) {
+            if(arr[i] <= arr[right]) {
+                swap(arr, i, pIndex);
+                pIndex++;
+            }
+        }
+        return pIndex - 1;
+        
+    }
+    
+    private void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
