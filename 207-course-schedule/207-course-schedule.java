35fbf1e3bd0edebe447cb266ddd4d5b6872c438a
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-08-27 16:45:13 -0700

    Time: 17 ms (23.08%), Space: 50.3 MB (15.89%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,12 +1,12 @@
 class Solution {
-    public boolean canFinish(int tasks, int[][] prerequisites) {
-        List<Integer> sortedOrder = new ArrayList<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer> schedule = new ArrayList<>();
 
-        HashMap<Integer, Integer> inDegree = new HashMap<>();
+        HashMap<Integer, Integer> appearances = new HashMap<>();
         HashMap<Integer, List<Integer>> graph = new HashMap<>();
     
-        for (int i = 0; i < tasks; i++) {
-            inDegree.put(i, 0);
+        for (int i = 0; i < numCourses; i++) {
+            appearances.put(i, 0);
             graph.put(i, new ArrayList<Integer>());
         }
 
@@ -14,35 +14,35 @@ class Solution {
         for (int i = 0; i < prerequisites.length; i++) {
             int parent = prerequisites[i][0], child = prerequisites[i][1];
             graph.get(parent).add(child);
-            inDegree.put(child, inDegree.get(child) + 1);
+            appearances.put(child, appearances.get(child) + 1);
         }
 
         // c. Find all sources i.e., all vertices with 0 in-degrees
         Queue<Integer> sources = new LinkedList<>();
-        for (Map.Entry<Integer, Integer> entry : inDegree.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : appearances.entrySet()) {
             if (entry.getValue() == 0)
                 sources.add(entry.getKey());
         }
 
-        // d. For each source, add it to the sortedOrder 
+        // d. For each source, add it to the schedule 
         // and subtract one from all of its 
         // children's in-degrees if a child's in-degree 
         // becomes zero, add it to sources queue
         while (!sources.isEmpty()) {
             int vertex = sources.poll();
-            sortedOrder.add(vertex);
+            schedule.add(vertex);
             // get the node's children to decrement their in-degrees
             List<Integer> children = graph.get(vertex);
             for (int child : children) {
-                inDegree.put(child, inDegree.get(child) - 1);
-                if (inDegree.get(child) == 0)
+                appearances.put(child, appearances.get(child) - 1);
+                if (appearances.get(child) == 0)
                     sources.add(child);
             }
         }
 
-        // if sortedOrder doesn't contain all tasks, 
+        // if schedule doesn't contain all tasks, 
         // there is a cyclic dependency between 
         // tasks, therefore, we will not be able to schedule all tasks
-        return sortedOrder.size() == tasks;
+        return schedule.size() == numCourses;
     }
 }
