bc163dd37b7383318d76021c904483da12c2ab22
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-08-28 13:01:19 -0700

    Time: 8 ms (59.64%), Space: 42.7 MB (93.50%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,49 +1,46 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer> schedule = new ArrayList<>();
-
-        HashMap<Integer, Integer> appearances = new HashMap<>();
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
-    
-        for (int i = 0; i < numCourses; i++) {
-            appearances.put(i, 0);
-            graph.put(i, new ArrayList<Integer>());
+        List<Integer> Schedule = new ArrayList<>();
+        
+        // Course i with how many prereqs are needed to take it
+        HashMap<Integer,Integer> numPreq = new HashMap<>();
+        
+        // Course i with List Corresponding to Other Courses Course i is a prereq of 
+        HashMap<Integer,List<Integer>> listOfPreq = new HashMap<>();
+        
+        // Initialize the Maps 
+        for(int i = 0; i < numCourses; i++) {
+            numPreq.put(i, 0);
+            listOfPreq.put(i, new ArrayList<Integer>());
         }
-
-        // b. Build the graph
-        for (int i = 0; i < prerequisites.length; i++) {
-            int parent = prerequisites[i][0]; 
-            int child = prerequisites[i][1];
-            graph.get(parent).add(child);
-            appearances.put(child, appearances.get(child) + 1);
+        
+        // Load the Maps with how many prereqs each courses has
+        for(int j = 0; j < prerequisites.length; j++) {
+            int course = prerequisites[j][1];
+            int prereq = prerequisites[j][0];
+            numPreq.put(course, numPreq.get(course) + 1);
+            listOfPreq.get(prereq).add(course);
         }
-
-        // c. Find all sources i.e., all vertices with 0 in-degrees
-        Queue<Integer> sources = new LinkedList<>();
-        for (Map.Entry<Integer, Integer> entry : appearances.entrySet()) {
-            if (entry.getValue() == 0)
-                sources.add(entry.getKey());
-        }
-
-        // d. For each source, add it to the schedule 
-        // and subtract one from all of its 
-        // children's in-degrees if a child's in-degree 
-        // becomes zero, add it to sources queue
-        while (!sources.isEmpty()) {
-            int vertex = sources.poll();
-            schedule.add(vertex);
-            // get the node's children to decrement their in-degrees
-            List<Integer> children = graph.get(vertex);
-            for (int child : children) {
-                appearances.put(child, appearances.get(child) - 1);
-                if (appearances.get(child) == 0)
-                    sources.add(child);
+        
+        Queue<Integer> order = new LinkedList<>();
+        for(Map.Entry<Integer,Integer> entry : numPreq.entrySet()) {
+            if(entry.getValue() == 0) {
+                order.add(entry.getKey());
             }
         }
-
-        // if schedule doesn't contain all tasks, 
-        // there is a cyclic dependency between 
-        // tasks, therefore, we will not be able to schedule all tasks
-        return schedule.size() == numCourses;
+        
+        while(!order.isEmpty()) {
+            int currentCourse = order.poll();
+            Schedule.add(currentCourse);
+            List<Integer> updateList = listOfPreq.get(currentCourse);
+            for(int c: updateList) {
+                numPreq.put(c, numPreq.get(c) -1);
+                if(numPreq.get(c) == 0) {
+                    order.add(c);
+                }
+            }
+        }
+        return Schedule.size() == numCourses;
+        
     }
 }
