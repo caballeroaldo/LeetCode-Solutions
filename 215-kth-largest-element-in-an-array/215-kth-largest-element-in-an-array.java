5f31c6bde635c192a9410b7dd118b1125ff1babf
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-08-28 13:22:12 -0700

    Time: 7 ms (82.64%), Space: 69.8 MB (52.75%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,30 +1,27 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+        return quickSelect(nums, 0, nums.length -1, nums.length - k);
     }
     
-    private int quickSelect(int[] nums, int left, int right, int kth) {
+    private int quickSelect(int[] nums, int left, int right, int k) {
         if(left == right) {
             return nums[left];
         }
         
         int pIndex = new Random().nextInt(right - left + 1) + left;
         pIndex = partition(nums, left, right, pIndex);
-        
-        if(pIndex == kth) {
+        if(pIndex == k) {
             return nums[pIndex];
-        } else if(pIndex < kth) {
-            return quickSelect(nums, pIndex + 1, right, kth);
+        } else if(pIndex < k) {
+            return quickSelect(nums, pIndex + 1, right, k);
         }
-        return quickSelect(nums, left, pIndex - 1, kth);
-        
+        return quickSelect(nums, left, pIndex - 1, k);
     }
     
     private int partition(int[] nums, int left, int right, int pIndex) {
         int pivot = nums[pIndex];
-        swap(nums, pIndex, right);
+        swap(nums, right, pIndex);
         pIndex = left;
-        
         for(int i = left; i <= right; i++) {
             if(nums[i] <= pivot) {
                 swap(nums, i, pIndex);
@@ -35,10 +32,9 @@ class Solution {
         return pIndex -1;
     }
     
-    private void swap(int[] nums, int x, int y) {
-        int temp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = temp;
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
-    
 }
