55bff76f0ff45c9718e28e3be8f24ea52e7b7400
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-09-07 15:50:40 -0700

    Time: 19 ms (19.62%), Space: 55.5 MB (26.28%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -7,6 +7,9 @@ class Solution {
             int prevStart = answer.get(answer.size() -1)[0];
             int currentStart = intervals[i][0];
             
+            // Merge when the current start interval is overlapping
+            // with previous end of interval, if so update the end, 
+            // otherwise add current interval
             if(currentStart <= answer.get(answer.size() - 1)[1]) {
                 answer.get(answer.size() - 1)[1] =
                     Math.max(answer.get(answer.size() - 1)[1], intervals[i][1]);
