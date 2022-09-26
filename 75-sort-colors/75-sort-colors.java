fe20b2a51b66464c39ace5c932aa211af3664eb8
caballeroaldo <69697382+caballeroaldo@users.noreply.github.com>
2022-09-26 14:17:38 -0700

    Time: 0 ms (100.00%), Space: 42.9 MB (13.08%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeros = 0;        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                swap(nums, i, zeros);
+                zeros++;
+            }
+        }
+        
+        int ones = zeros;
+        for(int j = 0; j < nums.length; j++ ) {
+            if(nums[j] == 1) {
+                swap(nums,j,ones);
+                ones++;
+            }
+        }
+        int twos = ones;
+        for(int k = 0; k < nums.length && twos < nums.length; k++) {
+            if(nums[k] == 2 ) {
+                swap(nums,k,twos);
+                twos++;
+            }
+        }
+    }
+    
+    private void swap(int[] n, int i, int j) {
+        int temp = n[i];
+        n[i] = n[j];
+        n[j] = temp;
+        
+    }
+}
